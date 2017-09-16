97cecd1078424ed49c0c93a6f810c372f200235b
Matin Hormati <mthormati@gmail.com>
2017-09-16 18:25:17 -0400

    Implement nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-grid-system'
-import { Route, Switch, Redirect } from 'react-router-dom'; 
+import { Route, NavLink, Redirect } from 'react-router-dom'; 
+
+import Flashcards from './Flashcards';
+import Practice from './Practice';
+import Sites from './Sites';
 
 import './App.css';
 
@@ -8,16 +12,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="top-strip"></div>
         <Container>
-          <Row>
-            <Col sm={6}>
-              Educept
+          <Row id="nav-row">
+            <Col md={6} className="title">
+              <span className="blue">Edu</span>cept
             </Col>
-            <Col sm={6}>
-              Place links here
-            </Col>
+            <Col lg={2} className="navLink"><NavLink to="/flashcards" activeClassName="activeLink">Flashcards</NavLink></Col>
+            <Col lg={2} className="navLink"><NavLink to="/practice" activeClassName="activeLink">Practice</NavLink></Col>
+            <Col lg={2} className="navLink"><NavLink to="/sites" activeClassName="activeLink">Sites</NavLink></Col>
           </Row>
         </Container>
+
+
+        <Route exact path="/" render={() => (
+          <Redirect to="/flashcards"/>
+        )}/>
+        <Route exact path="/flashcards/:uid" Component={Flashcards} />
+        <Route path="/practice/:uid" Component={Practice} />
+        <Route path="/sites/:uid" Component={Sites} />
       </div>
     );
   }
